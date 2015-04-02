43acf11c74d93c03ff81b5f5d6ce553a373aa72f
peteaq <peteaq@PeTeAQ-Linux-Laptop>
2015-04-02 20:11:03 +0200

    Initial commit.

--- ASD.Zadanie_1/src/Main.java
+++ ASD.Zadanie_1/src/Main.java
@@ -10,18 +10,24 @@ public class Main {
 		
 		Random generator = new Random();
 		
-		for (int i = 5; i < 10; i++){
-			
+		for (int i = 0; i < 10; i++)
+		{			
 			int l = generator.nextInt(100);
 			tablica1[i] = l;
-		
+			System.out.println("komorka " + i + " to " + tablica1[i]);
 		}
 		
-		System.out.println(tablica1[6]);
+		//for (int element : tablica1)
+		//	System.out.println(element);
 		
-		for (int i = 1; i < 10; i++){
-			
-			
+		for (int i = 1; i < 10; i++)
+		{
+			if (tablica1[i - 1] < tablica1[i])
+			{
+				System.out.println("i to " + i);
+				tablica2[i] = tablica1[i - 1];
+				System.out.println(tablica2[i]);
+			}			
 			
 		}
 	
